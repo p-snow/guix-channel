2d6890532cc9419ffe5c288a6567286e9fc86ec3
p-snow <6841207+p-snow@users.noreply.github.com>
2023-02-08 14:12:46 +0900

    add mpv-libarchive (mpv with libarchive enabled)

---
+++ p-snow/packages/video.scm
@@ -0,0 +1,12 @@
+(define-module (p-snow packages video)
+  #:use-module (guix packages)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages backup))
+
+(define-public mpv-libarchive
+  (package
+   (inherit mpv)
+   (name "mpv-libarchive")
+   (version "0.35.1")
+   (inputs (modify-inputs (package-inputs mpv)
+                          (prepend libarchive)))))
