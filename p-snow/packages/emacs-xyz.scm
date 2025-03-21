24bfab12544fc883ab06423cddb4351523d0ca38
p-snow <public@p-snow.org>
2025-03-21 22:19:59 +0900

    Add org-clock-convenience, org-tidy, Whole line, define-word, jaword and try

--- p-snow/packages/emacs-xyz.scm
+++ p-snow/packages/emacs-xyz.scm
@@ -20,7 +20,7 @@
     (origin
      (method git-fetch)
      (uri (git-reference
-           (url "https://github.com/p-snow/org-web-track")
+           (url "https://github.com/p-snow/org-web-track.git")
            (commit (string-append "v" version))))
      (file-name (git-file-name name version))
      (sha256
@@ -62,7 +62,7 @@ from web pages and web APIs.")
       (origin
        (method git-fetch)
        (uri (git-reference
-             (url "https://github.com/momomo5717/avy-migemo")
+             (url "https://github.com/momomo5717/avy-migemo.git")
              (commit commit)))
        (file-name (git-file-name name version))
        (sha256
@@ -77,3 +77,153 @@ completion method for characters, words, and the like based on a balanced
 decision tree.  With this package, Japanese characters are added to avy's
 targets.  Users can narrow down the decision tree using kana input.")
      (license license:gpl3+))))
+
+(define-public emacs-org-clock-convenience
+  (let ((revision "0")
+        (commit "5a9e32af581ecca0359de1815e3d37b563f9f8b2"))
+    (package
+     (name "emacs-org-clock-convenience")
+     (version (git-version "1.3" revision commit))
+     (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/dfeich/org-clock-convenience.git")
+             (commit commit)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1yhzmbflaw1gkg37i0ri6fxidqrkggfkl8mk5bq1s5xfd1jpp971"))))
+     (build-system emacs-build-system)
+     (arguments
+      (list
+       #:tests? #t
+       #:test-command #~(list "make" "test")))
+     (home-page "https://github.com/dfeich/org-clock-convenience")
+     (synopsis "Convenience functions for org time")
+     (description
+      "Convenience functions for easier time tracking. Provides commands for changing
+timestamps directly from the agenda view.")
+     (license license:gpl3+))))
+
+(define-public emacs-org-tidy
+  (let ((revision "0")
+        (commit "0bea3a2ceaa999e0ad195ba525c5c1dcf5fba43b"))
+    (package
+     (name "emacs-org-tidy")
+     (version (git-version "0.1" revision commit))
+     (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/jxq0/org-tidy.git")
+             (commit commit)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1rwq53j31vixyhsi7khb1xc0fcqdmqyp7ycq5hinligfxk87sr4s"))))
+     (propagated-inputs (list emacs-dash))
+     (build-system emacs-build-system)
+     (home-page "https://github.com/jxq0/org-tidy")
+     (synopsis "A minor mode to tidy org-mode buffers.")
+     (description
+      "A minor mode to tidy org-mode buffers.")
+     (license license:gpl3+))))
+
+(define-public emacs-whole-line-or-region
+  (let ((revision "0")
+        (commit "052676394c675303d6b953209e5d2ef090fbc45d"))
+    (package
+     (name "emacs-whole-line-or-region")
+     (version (git-version "2.0" revision commit))
+     (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/purcell/whole-line-or-region.git")
+             (commit commit)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "036x6nzij6h7s8ad89clx58hdkcw6kh31blhksdarwl7ssmi2ajg"))))
+     (build-system emacs-build-system)
+     ;; FIXME: Permission denied, /homeless-shelter
+     ;; (arguments
+     ;;  (list
+     ;;   #:tests? #t
+     ;;   #:test-command #~(list "make" "all")))
+     (home-page "https://github.com/purcell/whole-line-or-region")
+     (synopsis "Operate on current line if region undefined.")
+     (description
+      "This minor mode allows functions to operate on the current line if they would
+normally operate on a region and region is currently undefined.")
+     (license license:gpl3+))))
+
+(define-public emacs-define-word
+  (let ((revision "0")
+        (commit "31a8c67405afa99d0e25e7c86a4ee7ef84a808fe"))
+    (package
+     (name "emacs-define-word")
+     (version (git-version "0.1.0" revision commit))
+     (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/abo-abo/define-word.git")
+             (commit commit)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0h3dasg81f1b08xvz38nyd887pdlv60kj8q50kk2aqlpkr8j0y18"))))
+     (build-system emacs-build-system)
+     (home-page "https://github.com/abo-abo/define-word")
+     (synopsis "Display the definition of word at point.")
+     (description
+      "This package will send an anonymous request to https://wordnik.com/ to get the
+definition of word or phrase at point, parse the resulting HTML page, and
+display it with `message'.")
+     (license license:gpl3+))))
+
+(define-public emacs-jaword
+  (let ((revision "0")
+        (commit "783544a265f91b2e568b52311afb36e3691d5ad3"))
+    (package
+      (name "emacs-jaword")
+      (version (git-version "1.0.1" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/zk-phi/jaword.git")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "09gn08c9wz60jc8jh5y3bv7qymp0ciz62bk5sjwkwgnq5mkpsf49"))))
+      (build-system emacs-build-system)
+      (propagated-inputs (list emacs-tinysegmenter))
+      (home-page "http://zk-phi.github.io/")
+      (synopsis "Minor-mode for handling Japanese words")
+      (description
+       "This script provides a minor-mode that improves backward/forward-word behavior
+for Japanese words.")
+      (license license:gpl3+))))
+
+(define-public emacs-try
+  (let ((revision "0")
+        (commit "8831ded1784df43a2bd56c25ad3d0650cdb9df1d"))
+    (package
+      (name "emacs-try")
+      (version (git-version "0.0.1" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/larstvei/Try.git")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0y26ybdsljph49w2834wssxgdx8ij7b6v4gp8jpgnbx118gr4jsz"))))
+      (build-system emacs-build-system)
+      (home-page "https://github.com/larstvei/try")
+      (synopsis "Try out Emacs packages")
+      (description
+       "Try is a package that allows you to try out Emacs packages without installing
+them. If you pass a URL to a plain text `.el`-file it evaluates the content,
+without storing the file.")
+      (license license:gpl3+))))
